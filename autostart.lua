2782fa9bedc85b204df13e445ab7fd72fd8ec8c7
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2025-08-16 00:52:13 +0100

    🔧 Add autostart file with EasyEffects and Xrandr

---
+++ autostart.lua
@@ -0,0 +1,7 @@
+local awful = require("awful")
+
+-- equalizer
+awful.util.spawn_with_shell("easyeffects --gapplication-service &")
+
+-- dualmonitor
+awful.util.spawn_with_shell("xrandr --output eDP-1 --mode 1920x1080 --pos 0x360 --rotate normal --output HDMI-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal")
