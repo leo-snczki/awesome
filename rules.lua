2521dcadc9f1b13e66d7aa5787d5e1e96e136c69
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2025-08-19 04:50:04 +0100

    🔧 Center floating clients by default

--- rules.lua
+++ rules.lua
@@ -51,7 +51,11 @@ awful.rules.rules = {
                 "pop-up",        -- e.g. Google Chrome's (detached) Developer Tools.
             }
         },
-        properties = { floating = true }
+        properties = {
+            floating = true,
+            placement = awful.placement.centered
+        }
+
     },
 
     -- Add titlebars to normal clients and dialogs
