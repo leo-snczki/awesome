29f27766932efa22f0da74150266de314e66927c
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2025-08-19 03:09:33 +0100

    🚚 Rename 'keybinds.lua' to 'keyboard.lua'
    To help organizing later with a folder with everything about
    shortcuts/keys.

--- rc.lua
+++ rc.lua
@@ -19,7 +19,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
 require("autostart")
-require("keybinds")
+require("keyboard")
 require("variables")
 require("rules")
 require("mouse")
