8f01c11aae934a059b7291882a9e3c481845d1a2
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2025-08-20 05:00:52 +0100

    🔧 Move error handler from 'rc.lua' to 'error_handler.lua'

--- rc.lua
+++ rc.lua
@@ -9,49 +9,18 @@ require("awful.autofocus")
 
 -- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
+
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
-
+require("error_handler")
 require("autostart")
 require("hotkeys.keyboard")
 require("variables")
 require("rules")
 require("hotkeys.mouse")
 require("signals")
-
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({
-        preset = naughty.config.presets.critical,
-        title = "Oops, there were errors during startup!",
-        text = awesome.startup_errors
-    })
-end
-
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function(err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({
-            preset = naughty.config.presets.critical,
-            title = "Oops, an error happened!",
-            text = tostring(err)
-        })
-        in_error = false
-    end)
-end
--- }}}
-
 
 
 -- {{{ Menu
