7949888e1d468f4e9cb1c765fcc91974985514bf
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2025-09-18 22:18:43 +0100

    🔧 Set wacom digital table to only work on 'HDMI-1'
    Through 'xinput' due to 'xsetwacom' not working for me:
    $ xsetwacom set 14 MapToOutput HDMI-1
    Unable to find an output 'HDMI-1'

--- autostart.lua
+++ autostart.lua
@@ -5,3 +5,6 @@ awful.util.spawn_with_shell("easyeffects --gapplication-service &")
 
 -- dualmonitor
 awful.util.spawn_with_shell("xrandr --output eDP-1 --mode 1920x1080 --pos 0x360 --rotate normal --output HDMI-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal")
+
+-- wacom only on hdmi
+awful.util.spawn_with_shell("xinput map-to-output 14 HDMI-1") -- Check the id with xinput | grep "stylus"
